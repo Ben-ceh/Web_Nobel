0c037e0bd80ce1484708f64e21757478aa588708
Bence <b3niib@gmail.com>
2025-04-14 09:46:09 +0200

    Navbar és Nobel prize kész.

---
+++ menu.js
@@ -0,0 +1,21 @@
+document.getElementById("menu").innerHTML+=`
+<nav class="navbar navbar-expand-sm bg-light">
+
+    <div class="container-fluid">
+      <!-- Links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="https://en.wikipedia.org/wiki/Nobel_Prize">Nobel prize</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 3</a>
+        </li>
+      </ul>
+    </div>
+  
+  </nav>
+
+`
