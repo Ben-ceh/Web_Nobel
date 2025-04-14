63b545ad0be67e52259c4c38f672caa3445c0211
Bence <b3niib@gmail.com>
2025-04-14 09:47:42 +0200

    Nobel wiki javitas

--- menu.js
+++ menu.js
@@ -1,21 +1,24 @@
 document.getElementById("menu").innerHTML+=`
-<nav class="navbar navbar-expand-sm bg-light">
-
-    <div class="container-fluid">
-      <!-- Links -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Nobel díjak</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="https://en.wikipedia.org/wiki/Nobel_Prize">Nobel prize</a>
+          <a class="nav-link" href="https://en.wikipedia.org/wiki/Nobel_Prize">Nobel wiki</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link 2</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link 3</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
       </ul>
     </div>
-  
-  </nav>
+  </div>
+</nav>
 
 `
