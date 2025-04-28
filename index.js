a82338a441903002ea428185490e8eec14ad12df
Bence <b3niib@gmail.com>
2025-04-28 09:18:32 +0200

    Modalis torzs kesz

--- index.js
+++ index.js
@@ -29,4 +29,17 @@ function reszletKiir(sorszam){
     console.log(tomb)
     // alert(JSON.stringify(tomb))
     document.getElementById("modalFej").innerHTML=tomb.prizes[sorszam].year+ " " + tomb.prizes[sorszam].category
+let sz = `<ol>`
+  for (const elem of tomb.prizes[sorszam].laureates) {
+      sz+=`<li>
+        <p>
+        ${elem.firstname} ${elem.surname}
+        </p>
+        <p>
+        ${elem.motivation} 
+        </p>
+            </li>`
+  }
+  sz+=`</ol>`
+  document.getElementById("modalTorzs").innerHTML+=sz
 }
